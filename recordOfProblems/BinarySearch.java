d4740fc31c8d6a9265965dec5a7046caa8d3ab68
José Joaquín Arias <joaquinariasgomezcal@gmail.com>
2022-01-19 19:26:33 +0100

    Modified with searches for leftmost and rightmost

--- recordOfProblems/BinarySearch.java
+++ recordOfProblems/BinarySearch.java
@@ -18,4 +18,45 @@ public class BinarySearch {
 
         return -1;
     }
+
+    public int searchLeftMost(int[] nums, int target) {
+        int begin = 0;
+        int end = nums.length - 1;
+
+        while(begin < end) {
+            int mid = begin + (end-begin)/2;
+            if(nums[mid] < target) {
+                begin = mid+1;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return begin;
+    }
+
+    public int searchRightMost(int[] nums, int target) {
+        int begin = 0;
+        int end = nums.length - 1;
+
+        while(begin < end) {
+            int mid = begin + (end-begin)/2;
+            if(nums[mid] > target) {
+                end = mid;
+            }
+            else {
+                begin = mid + 1;
+            }
+        }
+        return end - 1;
+    }
+
+    public void run() {
+        int[] nums = {1, 2, 2, 2, 5, 6};
+        System.out.println(searchRightMost(nums, 2));
+    }
+
+    public static void main(String[] args) {
+        new BinarySearch().run();
+    }
 }
