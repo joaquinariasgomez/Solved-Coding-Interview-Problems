157038728fff66a73615fefbba78de096808b0bb
José Joaquín Arias <joaquinariasgomezcal@gmail.com>
2022-05-03 07:01:47 +0200

    Improved solution

--- recordOfProblems/c++/IsUnique.cc
+++ recordOfProblems/c++/IsUnique.cc
@@ -15,7 +15,24 @@ bool hasUniqueChars(std::string input) {
     return result;
 }
 
+bool hasUniqueChars2(std::string input) {
+    bool result = true;
+    bool charSet[128];
+
+    for(int i=0; i<128; i++) {
+        charSet[i] = false;
+    } 
+
+    for(int i=0; i<input.length(); i++) {
+        if(charSet[input[i]]) {
+            return false;
+        }
+        charSet[input[i]] = true;
+    }
+    return result;
+}
+
 int main() {
     std::string input = "holaa";
-    std::cout << (hasUniqueChars(input) ? "Has unique chars" : "Has NOT unique chars") << std::endl;
+    std::cout << (hasUniqueChars2(input) ? "Has unique chars" : "Has NOT unique chars") << std::endl;
 }
