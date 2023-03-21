60ac37376b2203194c284a566bc1c034229b6fcf
Joaquín Arias <joaquinariasgomezcal@gmail.com>
2023-03-21 09:38:08 +0100

    Moar things

--- recordOfProblems/CheckPermutation.java
+++ recordOfProblems/CheckPermutation.java
@@ -2,6 +2,12 @@ import java.util.HashMap;
 
 public class CheckPermutation {
 
+    public void printMap(HashMap<Character, Integer> map) {
+        for(HashMap.Entry<Character, Integer> entrySet : map.entrySet()) {
+            System.out.println("Key "+entrySet.getKey()+" value: "+entrySet.getValue());
+        }
+    }
+
     public boolean checkCharacterCounts(String string1, String string2) {
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         if(string1.length() != string2.length()) return false;
@@ -11,6 +17,7 @@ public class CheckPermutation {
         for(int i=0; i<array1.length; i++) {
             map.put(array1[i], map.getOrDefault(array1[i], 0)+1);
         }
+//        printMap(map);
         for(int i=0; i<array2.length; i++) {
             map.put(array2[i], map.getOrDefault(array2[i], 0)-1);
             if(map.get(array2[i])<0) {
@@ -21,8 +28,8 @@ public class CheckPermutation {
     }
 
     public void run() {
-        String string1 = "abcba";
-        String string2 = "aabc";
+        String string1 = "abdd";
+        String string2 = "adcb";
         System.out.println(checkCharacterCounts(string1, string2));
     }
 
